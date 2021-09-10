523815ad9699467efbd0cdc877f36018f2043aa7
James Wilkinson <james.wilkinson@couchbase.com>
2021-09-10 16:38:01 +0100

    Added Update support for buckets

--- couchbasecloud/buckets.go
+++ couchbasecloud/buckets.go
@@ -58,6 +58,21 @@ func (client *CouchbaseCloudClient) CreateBucket(cluster *Cluster, payload *Buck
 	return nil
 }
 
+func (client *CouchbaseCloudClient) UpdateBucket(cluster *Cluster, payload []*BucketCreatePayload) error {
+	cloudsUrl := client.BaseURL + client.getApiEndpoint("/clusters/"+cluster.Id+"/buckets")
+
+	req, err := http.NewRequest(http.MethodPut, cloudsUrl, nil)
+	if err != nil {
+		return err
+	}
+
+	if err := client.sendRequest(req, payload, true); err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func (client *CouchbaseCloudClient) DeleteBucket(cluster *Cluster, payload *BucketDeletePayload) error {
 	cloudsUrl := client.BaseURL + client.getApiEndpoint("/clusters/"+cluster.Id+"/buckets")
 
