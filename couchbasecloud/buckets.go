59b0ce3c1b769271e1feedb5cd07d025551e8298
James Wilkinson <james.wilkinson@couchbase.com>
2021-09-10 15:18:21 +0100

    Added List support for buckets

--- couchbasecloud/buckets.go
+++ couchbasecloud/buckets.go
@@ -4,6 +4,9 @@ import (
 	"net/http"
 )
 
+type BucketsList struct {
+}
+
 type BucketCreatePayload struct {
 	Name        string `json:"name"`
 	MemoryQuota int    `json:"memoryQuota"`
@@ -11,6 +14,23 @@ type BucketCreatePayload struct {
 
 type BucketDeletePayload struct {
 	Name string `json:"name"`
+}
+
+func (client *CouchbaseCloudClient) ListBuckets(cluster *Cluster) (*BucketsList, error) {
+	cloudsUrl := client.BaseURL + client.getApiEndpoint("/clusters/"+cluster.Id+"/buckets")
+
+	req, err := http.NewRequest(http.MethodGet, cloudsUrl, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	res := BucketsList{}
+
+	if err := client.sendRequest(req, &res, true); err != nil {
+		return nil, err
+	}
+
+	return &res, nil
 }
 
 func (client *CouchbaseCloudClient) CreateBucket(cluster *Cluster, payload *BucketCreatePayload) error {
